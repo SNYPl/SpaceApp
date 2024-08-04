f2be996cbf7aa93f28a556894f2156b54a127fcd
Davit Metreveli <davitmetreveli@pulsarai.ge>
2024-08-04 18:15:08 +0400

    change image quality

--- src/components/offers/offer/Offer.jsx
+++ src/components/offers/offer/Offer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./offer.scss";
 import Button from "../../button/Button";
+import shape from "../../../asset/images/offers/shape.png";
 
 const Offer = ({ title, paragraph, image }) => {
   return (
     <article className="offer" style={{ backgroundImage: `url(${image})` }}>
+      <img src={shape} alt="shape" className="offer--shape" />
       <h2 className="offer--title">{title}</h2>
       <p className="offer--paragraph">{paragraph}</p>
       <Button className="offer--button">Learn more</Button>
